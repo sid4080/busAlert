2add427f0fd6a85b23c9d0838592ea8fb2f4d042
SIDDHARTH GUPTA <SIDDHARTH GUPTA>
2018-06-25 22:59:28 +0530

    email using google SMTP server

--- src/main/java/main/BusService.java
+++ src/main/java/main/BusService.java
@@ -3,10 +3,7 @@ package main;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.time.LocalDate;
@@ -16,25 +13,36 @@ import java.util.Properties;
 @Service
 public class BusService {
 
-public void alertUser (LocalDate  date) throws MessagingException {
-    Integer seatCount=getAvailableSeats(date);
-    if (seatCount<50)
-    {
-        Properties properties=new Properties();
-        properties.setProperty("mail.smtp.port","25");
-        Session session =Session.getDefaultInstance(properties);
-        MimeMessage mimeMessage= new MimeMessage(session);
-        mimeMessage.setSubject("book your seats","UTF-8");
-        mimeMessage.setText("Seat book karlo bro "+seatCount+" bachi h","UTF-8");
-        mimeMessage.setRecipients(Message.RecipientType.TO,InternetAddress.parse("siddisgupta@gmail.com" ));
-        Transport.send(mimeMessage);
+    public void alertUser(LocalDate date) throws MessagingException {
+        Integer seatCount = getAvailableSeats(date);
+        if (seatCount < 50) {
+            Properties props = System.getProperties();
+            String host = "smtp.gmail.com";
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.user", "shivi301193");
+            props.put("mail.smtp.password", "busalert");
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+
+            Session session = Session.getDefaultInstance(props,new javax.mail.Authenticator(){
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("shivi301193@gmail.com","busalert");
+                }
+            });
+
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setSubject("book your seats", "UTF-8");
+            mimeMessage.setText("Seat book karlo bro " + seatCount + " bachi h", "UTF-8");
+            mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse("siddisgupta@gmail.com"));
+            Transport.send(mimeMessage);
+        }
+
     }
 
-}
-
-    public Integer getAvailableSeats(LocalDate date)
-    {
-        RestTemplate restTemplate= new RestTemplate();
+    public Integer getAvailableSeats(LocalDate date) {
+        RestTemplate restTemplate = new RestTemplate();
         BusTicketResponse busTicketResponse = restTemplate.getForObject("https://busapi.mobikwik.com/api/v2/availability/5042/6727/" + date, BusTicketResponse.class);
         List<SearchResult> search_results = busTicketResponse.getData().getSearch_results();
         SearchResult searchResult = search_results.stream().filter(result -> result.getDep_time().equals("19:30")).findFirst().get();
